9072869a82e9175df54fc1a420ce56e69103c75b
purrre <purrre@proton.me>
2025-12-04 16:08:27 -0600

    v1.6.2 - refactor ping & add apistats

--- commands/info/utils.py
+++ commands/info/utils.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import bridge, commands
 
-import asyncio, time
+import time, httpx
 
 from functions.functions import httpcall, loading, colors
 
@@ -11,13 +11,15 @@ class UtilsCog(commands.Cog):
         self.region = None
         self.country = None
 
+
     @bridge.bridge_command(
         usage='ping',
         description='Pings juicewrldapi to check the status',
         aliases=['p'])
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def ping(self, ctx):
-        msg = await ctx.reply(embed=await loading('TCPing'))
+        msg = await ctx.reply(embed=await loading('PING'))
+
         if not self.country or self.country.lower() == 'unknown':
             ip = await httpcall('https://api.ipify.org', expect_json=False)
             r = await httpcall(f'https://ipinfo.io/{ip[1]}/json')
@@ -27,40 +29,87 @@ class UtilsCog(commands.Cog):
         embed = discord.Embed(title='juicewrldapi Status', url='https://downforeveryoneorjustme.com/juicewrldapi.com', color=colors.main)
 
         hosts = {
-            'main': 'juicewrldapi.com',
-            'media (master)': 'm.juicewrldapi.com'
+            'main': ('https://juicewrldapi.com', ['/']),
+            'api': ('https://juicewrldapi.com', ['/juicewrld/']),
+            'media (master)': ('https://m.juicewrldapi.com', ['/status/'])
         }
 
-        for name, host in hosts.items():
-            start = time.perf_counter()
+        async with httpx.AsyncClient(timeout=8.0) as client:
+            for name, (base_url, paths) in hosts.items():
+                start = time.perf_counter()
+                online = False
+                used_url = None
 
-            try:
-                reader, writer = await asyncio.wait_for(
-                    asyncio.open_connection(host, 80),
-                    timeout=3
-                )
-
-                writer.close()
-                if hasattr(writer, 'wait_closed'):
-                    await writer.wait_closed()
+                for p in paths:
+                    url = f'{base_url}{p}'
+                    try:
+                        r = await client.get(url)
+                        if r.status_code < 400:
+                            online = True
+                            used_url = url
+                            break
+                    except Exception:
+                        continue
 
                 ping_ms = (time.perf_counter() - start) * 1000
 
-                embed.add_field(
-                    name=f'<:status_online:1443125660552921142> {name.title()}',
-                    value=f'Ping: {ping_ms:.2f} ms',
-                    inline=True
-                )
-
-            except Exception as e:
-                embed.add_field(
-                    name=f'<:status_offline:1443125840501014572> {name.title()}',
-                    value=f'Error: `{type(e).__name__}`\nDetails: `{str(e)}`',
-                    inline=True
-                )
+                if online:
+                    embed.add_field(
+                        name=f'<:status_online:1443125660552921142> {name.title()}',
+                        value=f'Ping: {ping_ms:.2f} ms',
+                        inline=True
+                    )
+                else:
+                    embed.add_field(
+                        name=f'<:status_offline:1443125840501014572> {name.title()}',
+                        value=f"Couldn't reach host :(",
+                        inline=True
+                    )
 
         embed.set_footer(text=f'{self.bot.user.name} | Pinged from {self.region}, {self.country}', icon_url=self.bot.user.avatar.url)
+
         await msg.edit(embed=embed)
+
+
+
+    @bridge.bridge_command(
+        usage='apistats',
+        description='Shows basic juicewrldapi statistics',
+    )
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def apistats(self, ctx):
+        msg = await ctx.reply(embed=await loading('API'))
+        success1, mAPI = await httpcall('https://m.juicewrldapi.com/status/')
+        success2, gAPI = await httpcall('https://juicewrldapi.com/juicewrld/plays/stats/')
+        if not success1 or not success2:
+            return await ctx.reply(embed=mAPI)
+
+        embed = discord.Embed(title='juicewrldapi Statistics', color=colors.main, description=f'Total Files: {mAPI.get('total_files', 'N/A')} | Commits: {mAPI.get('total_commits', 'N/A')} | Data Size: {round(int(mAPI.get('total_size_bytes', '0')) / 1073741824 , 2)} GB', url='https://juicewrldapi.com/stats')
+        embed.set_thumbnail(url='https://raw.githubusercontent.com/HackinHood/juicewrldapi-desktop/refs/heads/master/assets/icon.png')
+        embed.add_field(name='__Plays__', value=f'- Total Plays: {gAPI.get('total_plays', 'N/A')}'
+                        f'\n- Unique Songs Played: {gAPI.get('total_songs_with_plays', 'N/A')}'
+                        f'\n- Unique Albums Played: {gAPI.get('total_albums_with_plays', 'N/A')}'
+                        f'\n- Unique Eras Played: {gAPI.get('total_eras_with_plays', 'N/A')}',
+                        inline=True)
+        
+        embed.add_field(
+            name='__Top Categories__',
+            value='\n'.join([f'- {c['category'].replace('_', ' ').title()}: {c['count']} plays' for c in sorted(gAPI.get('category_breakdown', []), key=lambda x: x['count'], reverse=True)[:5]]),
+            inline=True
+        )
+
+        embed.add_field(
+            name='__Top Songs__',
+            value='\n'.join([
+                f'- {s['name']} ({s['era_name']}): **{s['play_count']}** plays'
+                for s in sorted(gAPI.get('top_songs', []), key=lambda x: x['play_count'], reverse=True)[:5]
+            ]),
+            inline=False
+        )
+
+        embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar.url)
+        await msg.edit(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(UtilsCog(bot))
